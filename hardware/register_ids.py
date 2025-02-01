e4b92a39b0bc20a05ed6d4e7319e91049c2aa10e
Sriharsha Kavuri <sriharshakavuri1@gmail.com>
2025-02-01 14:50:22 -0800

    register ids done

---
+++ hardware/register_ids.py
@@ -0,0 +1,26 @@
+from time import sleep
+from mfrc522 import SimpleMFRC522 as mfr
+import csv
+
+
+reader = mfr()
+
+uid = {}
+
+try:
+    while True:
+        print("waiting for read")
+    
+        id, text = reader.read()
+        print(id)
+
+        uid[input("Enter name: ")] = id
+
+except KeyboardInterrupt:
+    if input('q!'):
+        exit()
+
+    with open('uid.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['name', 'uid'])
+        writer.writeheader()
+        writer.writerows(cars)
