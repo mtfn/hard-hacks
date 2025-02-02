f0ec485bbbcaaba53cffdea1e0920d4203980083
3dcantaloupe <pranavdgs@gmail.com>
2025-02-01 20:51:56 -0800

    update mobile model and requirements

--- src/mobileModel.py
+++ src/mobileModel.py
@@ -18,17 +18,37 @@ def is_recyclable(image_path):
     decoded_predictions = decode_predictions(predictions, top=3)[0]
 
     # Check if any of the top predictions are recyclable items
-    recyclable_items = ['bottle', 'can', 'carton', 'paper_towel', 'plastic_bag']
+    recyclable_items = [
+    'bottle', 'can', 'carton', 'paper_towel', 'plastic_bag', 'paper_bag', 
+    'paper', 'cardboard', 'glass_jar', 'plastic_container', 'aluminum_foil', 
+    'newspaper', 'plastic_bottle', 'food_packaging', 'magazines', 'egg_cartons', 
+    'phone_books', 'milk_cartons', 'tin_can', 'pizza_box', 'tissue_box', 
+    'wrapping_paper', 'glass_bottle', 'straws', 'plastic_lid', 'yogurt_container', 
+    'detergent_bottle', 'cereal_box', 'soda_can', 'juice_box', 'plastic_wrap', 
+    'toilet_paper_roll', 'battery', 'shoebox', 'plastic_wrap', 'styrofoam', 
+    'broken_mirror', 'light_bulb', 'old_clothing', 'fabric_scraps', 'tote_bag', 
+    'plastic_tray', 'clamshell_container', 'electronics', 'printer_ink_cartridges', 
+    'soda_cans', 'takeout_container', 'cardboard_box', 'paint_can', 'aluminum_can', 
+    'metal_foil', 'furniture', 'coffee_cups', 'wood', 'furniture', 'broken_appliances', 
+    'foam_peanuts', 'shampoo_bottle', 'toothpaste_tube', 'plastic_knife_fork_spoon']
+
     for _, label, _ in decoded_predictions:
         if label in recyclable_items:
             return True
     return False
 
-# Path to the image
-image_path = './images/big.jpg'
 
-# Check if the item in the image is recyclable
-if is_recyclable(image_path):
-    print("The item in the image is recyclable.")
-else:
-    print("The item in the image is not recyclable.")
+
+def main():
+    # Path to the image
+    image_path = './images/big.jpg'
+
+    # Check if the item in the image is recyclable
+    if is_recyclable(image_path):
+        print("The item in the image is recyclable.")
+    else:
+        print("The item in the image is not recyclable.")
+
+if __name__ == '__main__':
+    main()
+
